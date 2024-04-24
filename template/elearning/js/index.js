cebf90737d4d6678818e21f366cacd4e6a2d7c5f
tungds <tung.ds@buv.edu.vn>
2024-04-24 16:24:03 +0700

    update cod

---
+++ template/elearning/js/index.js
@@ -0,0 +1,1 @@
+import IndexClass from "./customIndex.js"
--- template/elearning/js/login.js
+++ template/elearning/js/login.js
@@ -1,36 +1,50 @@
-import { Validator } from "./function.js";
-import sheet from '../css/style.css' assert { type: 'css' };
-document.adoptedStyleSheets = [sheet];
+import * as CSS from "./stylessheet.js";
+import { Validator } from "./validator.js";
+import { setTitle } from "./head.js";
+import * as dasboard from "./dashboard.js";
 
-Validator({
-    form: '#modal__login-form',
-    rules: [
-        Validator.tbRequired({
-            selector : '#form__tb--username', 
-            msg : Validator.message.required
-        }),
-        Validator.tbRequired({
-            selector : '#form__tb--password', 
-            msg : Validator.message.required
-        })
-    ],
-    onSubmit: (data) => {
-        document.getElementById("modal__login").style.display = "none";
-        document.getElementById("modal__loading").style.display = "flex";
-    }
-});
+setTitle('Login Page');
 
-function render(){
-    let rootElement = document.getElementById('root');
+async function htmlRender(){
+    let h = '<div class="modal modal__login" id="modal__login" style="display: flex;">';
+            h += '<div class="modal__overlay"></div>';
+            h += '<div class="modal__content modal__content--login">';
+                h += '<img class="modal__login-logo" src="img/tungbinhthaison-thuphap.png" alt="tungbinhthaison-thuphap.png">';
+                h += '<form class="modal__login-form" id="modal__login-form" action="">';
+                    h += '<div class="form__group validate">';
+                        h += '<input class="form__tb" id="form__tb--username" type="text" placeholder="Tài khoản">';
+                        h += '<small class="message__error"></small>';
+                    h += '</div>';
+                    h += '<div class="form__group validate">';
+                        h += '<input class="form__tb" id="form__tb--password" type="text" placeholder="Mật khẩu">';
+                        h += '<small class="message__error"></small>';
+                    h += '</div>';
+                    h += '<input class="form__tb form__tb--submit" type="submit" value="Đăng nhập">';
+                h += '</form>';
+            h += '</div>';
+        h += '</div>';
 
-    let headings = [1, 2, 3, 4];
-
-    headings.forEach( index => {
-        let h1 = document.createElement('h1');
-        h1.classList.add('heading-hello');
-        h1.innerText = 'Hello world ' + index;
-        rootElement.appendChild(h1);
-    });
+    document.getElementById('root').innerHTML = h;
 }
 
-render();
+htmlRender().then(
+    Validator({
+        form: '#modal__login-form',
+        rules: [
+            Validator.tbRequired({
+                selector : '#form__tb--username', 
+                msg : Validator.message.required
+            }),
+            Validator.tbRequired({
+                selector : '#form__tb--password', 
+                msg : Validator.message.required
+            })
+        ],
+        onSubmit: (data) => {
+            document.getElementById("modal__loading").style.display = "flex";
+            window.history.pushState('dasboard', 'Title', '/dasboard');
+        }
+    })
+);
+
+
